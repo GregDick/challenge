913c085296bf96f72e731bea1d204310efd10c26
Greg Dick <gdick@mercuryintermedia.com>
2019-05-31 20:29:26 -0500

    starting search bar

--- app/src/main/java/com/example/greg/challenge/SearchActivity.kt
+++ app/src/main/java/com/example/greg/challenge/SearchActivity.kt
@@ -2,11 +2,20 @@ package com.example.greg.challenge
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_search.*
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+        return true
     }
 }
