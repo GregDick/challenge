792506f2543e3a056958df13cbe660d27d081ee4
Greg Dick <gdick@mercuryintermedia.com>
2019-06-09 22:55:56 -0500

    search activity tests

--- app/src/androidTest/java/com/example/greg/challenge/SearchActivityTest.kt
+++ app/src/androidTest/java/com/example/greg/challenge/SearchActivityTest.kt
@@ -1,13 +1,20 @@
 package com.example.greg.challenge
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
 import com.example.greg.challenge.model.Repo
 import com.example.greg.challenge.view.SearchActivity
 import junit.framework.TestCase
+import org.hamcrest.Matchers.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
@@ -25,15 +32,46 @@ class SearchActivityTest : TestCase() {
         activity = activityRule.activity
     }
 
-
-
-    private fun mockRepoList(): ArrayList<Repo> {
-        val testRepo = mockRepo()
-        return arrayListOf(testRepo, testRepo, testRepo, testRepo, testRepo, testRepo, testRepo, testRepo)
+    @Test
+    fun testWelcomeView() {
+        onView(withId(R.id.welcome_message)).check(matches(isDisplayed()))
     }
 
-    private fun mockRepo() : Repo {
-        return Repo("test", null, "test", 0, 0, 0, "test")
+    @Test
+    fun testSearchViewDisplaysRecyclerView() {
+        //execute
+        onView(withId(R.id.search_src_text)).perform(typeText("TEST"))
+
+        Thread.sleep(2500) //idling resource would be better
+
+        //assert
+        onView(withId(R.id.welcome_message)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.results_recycler_view)).check(matches(isDisplayed()))
     }
 
+    @Test
+    fun testSearchViewDisplaysNoResults() {
+        //execute
+        onView(withId(R.id.search_src_text)).perform(typeText(",.;/;[[lasjja"))
+
+        Thread.sleep(2500) //idling resource would be better
+
+        //assert
+        onView(withId(R.id.welcome_message)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.results_recycler_view)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.no_results_view)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testSearchViewBlankQueryDisplaysNoResults() {
+        //execute
+        onView(withId(R.id.search_src_text)).perform(typeText(" "))
+
+        Thread.sleep(750) //idling resource would be better
+
+        //assert
+        onView(withId(R.id.welcome_message)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.results_recycler_view)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.no_results_view)).check(matches(isDisplayed()))
+    }
 }
